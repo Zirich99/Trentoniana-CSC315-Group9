9ffafa17f4c1385fb184cf0a89e82089216d982c
Michael Mongelli <mongelm1@tcnj.edu>
2021-04-23 14:24:52 -0400

    tested getting file from csv, added Ally's file and working to put its data into the database

---
+++ app/test.sql
@@ -0,0 +1,17 @@
+-- lion=> \i full/path/to/this/file.sql
+
+DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+
+-- connect user to new database
+\c test
+
+CREATE TABLE TEST_TABLE (
+    a INT NOT NULL PRIMARY KEY,
+    b INT NOT NULL,
+    c INT NOT NULL
+);
+
+-- you can say TEST_TABLE or TEST_TABLE(a, b, c) to specify columns
+\copy TEST_TABLE(a, b, c) FROM '/home/lion/Desktop/app/sample.csv' DELIMITER ',' CSV HEADER
