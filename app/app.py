2e52273f373c106701b3ae0c0eb4cc6192f95d0c
Michael Mongelli <mongelm1@tcnj.edu>
2021-04-25 22:12:03 -0400

    Search functionality is done. THe search function takes in a user's search string from a text box on the page. When they click submit, this string is sent to the search() function which gets all relevant string fields from the trentoniana database and puts them into a table. The search() function then looks at all the strings in the table and highlights matches by surrounding them with < >. If the user searches for the empty string, no entries are returned.

--- app/app.py
+++ app/app.py
@@ -54,9 +54,84 @@ https://www.geeksforgeeks.org/python-using-for-loop-in-flask/
 import psycopg2
 from config import config
 from flask import Flask, render_template, request
-import sys
- 
-#You will need to add a function for each query you want to make.
+
+# Functions to handle user input
+def search(userstr):
+    # input: substr the user enters into the search bar
+    # output: ENTRYs containing the user's search string in any of its fields
+    
+    # This function searches for user's input string in the fields:
+    # ENTRY.entry_name
+    # AUDIOFILE.audiofile_name
+    # TRANSCRIPTFILE.transcriptfile_name
+    # CATEGORY.c_name
+    # PARTICIPANT.fullname
+    # TRANSCRIBER.fullname
+    # where all these values are joined by the ENTRY they relate to
+
+    query = (
+        """
+SELECT
+    ENTRY.entry_name AS entry,
+    CATEGORY.c_name AS category,
+    AUDIOFILE.audio_filename AS audio,
+    PARTICIPANT.fullname AS participant,
+    TRANSCRIPTFILE.transcript_filename AS transcript,
+    TRANSCRIBER.fullname AS transcriber
+
+FROM ENTRY
+
+-- get category of entry
+JOIN CATEGORY
+    ON ENTRY.c_name = CATEGORY.c_name
+
+-- get audio filename and participants
+JOIN AUDIO
+    ON ENTRY.audio_id = AUDIO.audio_id
+JOIN AUDIOFILE
+    ON AUDIO.audiofile_id = AUDIOFILE.audiofile_id
+JOIN PARTICIPANT
+    ON AUDIO.p_id = PARTICIPANT.p_id
+
+-- get transcript filename and transcriber
+JOIN TRANSCRIPT
+    ON ENTRY.transcript_id = TRANSCRIPT.transcript_id
+JOIN TRANSCRIPTFILE
+    ON TRANSCRIPT.transcriptfile_id = TRANSCRIPTFILE.transcriptfile_id       
+JOIN TRANSCRIBER
+    ON TRANSCRIPT.t_id = TRANSCRIBER.t_id
+;
+        """
+    ).strip() # removes leading and trailing whitespace from this string
+
+    rows = connect(query)
+
+    # get rows containing the user string
+    rows = [row for row in rows if userstr in ''.join(row) and userstr!='']
+
+    if userstr=='':
+        # user searched for empty string
+        msg = [(f"You did not search for anything")]
+        rows.insert(0, msg)
+    elif rows:
+        # matches were found
+
+        # highlight matches by surrounding them with < >
+        rows = [(attr.replace(userstr, f'<{userstr}>') for attr in row) for row in rows]
+
+        header = ('Entry', 'Category', 'Audio', 'Participant', 'Transcript', 'Transcriber')
+        #cols = zip(*rows)
+        #header = (attr+('_'*len(max(col,key=len))) for (attr,col) in zip(header,cols))
+        rows.insert(0, header)
+    elif rows==[]:  
+        # no matches were found
+        msg = [(f"No entries were found containing your search '{userstr}'")]
+        rows.insert(0, msg)
+    else:
+        exit("idk what this case means")
+
+    return rows
+
 def connect(query):
     """ Connect to the PostgreSQL database server """
     conn = None
@@ -110,13 +185,11 @@ def form():
 @app.route('/form-handler', methods=['POST'])
 def handle_data():
     # user input fields
-    search = request.form['user_search']
+    user_search = request.form['search']
 
     # tables whose fields you can search for a user's substring
-
-    user_tables = [] # tables the user has chosen to search for their string
     
-    tables = (', ').join(request.form.getlist("search_substr"))
+    '''tables = (', ').join(request.form.getlist("search_substr"))
     if tables != '':
 
         # final query
@@ -125,7 +198,9 @@ def handle_data():
         # perform query
         rows = connect(query)
     else:
-        rows = []
+        rows = []'''
+
+    rows = search(user_search)
 
     return render_template('my-result.html', rows=rows)
 
